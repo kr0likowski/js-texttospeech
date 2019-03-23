9bb1f4ec619d59632d5b20929314af35b646efa0
Jedrzej Krolikowski <jedrzejkrolikowski@MacBook-Air-Jedrzej.local>
2019-03-23 17:14:30 +0100

    Populate voices function

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 var synth = window.speechSynthesis;
+
+var voiceSelect = document.querySelector('#voice-select');
+
+// DOM Loaded
+document.addEventListener('DOMContentLoaded',()=>{
+  populateVoices();
+});
+
+function populateVoices(){
+  voices = synth.getVoices();
+  voices.forEach((voice)=>{
+    let option = document.createElement('option');
+    option.textContent = voice.name + ' (' + voice.lang + ')';
+
+    if(voice.default){
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voice.lang);
+    option.setAttribute('data-name', voice.name);
+
+    voiceSelect.appendChild(option);
+  });
+}
